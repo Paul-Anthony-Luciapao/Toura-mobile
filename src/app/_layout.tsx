64cd3fc63a1e4f0ba931d7865afdd7fc4f7aba3b
JonBaldwinAlmarez123 <almarezjbm@gmail.com>
2026-09-23 18:51:32 +0800

    chore: update dependencies and configure Tailwind CSS with NativeWind
    - Added @expo-google-fonts/poppins for font management
    - Introduced nativewind for utility-first styling
    - Configured Tailwind CSS with a new global.css file
    - Set up babel.config.js for NativeWind integration
    - Updated tsconfig.json to include nativewind-env.d.ts
    - Created metro.config.js for Metro bundler with NativeWind support
    - Added tailwind.config.js for Tailwind CSS configuration

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,16 @@
 import AnimatedSplash from "@/components/common/AnimatedSplash";
 import {
-    Poppins_400Regular,
-    Poppins_500Medium,
-    Poppins_600SemiBold,
-    Poppins_700Bold,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
 } from "@expo-google-fonts/poppins";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect, useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import "../../global.css";
 
 export default function RootLayout() {
   const [showSplash, setShowSplash] = useState(true);
